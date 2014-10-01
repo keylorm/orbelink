65b8eb676dee065491ec1ee995ed948489769afc
Keylor Mora <keylor@orbelink.com>
2014-10-01 15:16:49 -0600

    CAmbio titulo

--- sites/all/themes/orbelink/templates/system/page--blog.tpl.php
+++ sites/all/themes/orbelink/templates/system/page--blog.tpl.php
@@ -106,7 +106,7 @@ global $base_url;
     <a id="main-content"></a>
     <div class="title-box">
       <?php print render($title_prefix); ?>
-      <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+      <?php if ($title): ?><p class="page-title-blog" >Nuestro <em class="subtitle">blog</em></p><?php endif; ?>
       <?php print render($title_suffix); ?>
     </div>
     <div class="main" role="main">
