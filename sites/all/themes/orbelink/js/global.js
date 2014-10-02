9f7eb22862482db47608b800c84f30fe8438c32e
Andrey <andreys2009@gmail.com>
2014-10-02 15:50:22 -0600

    Css de la tarde

--- sites/all/themes/orbelink/js/global.js
+++ sites/all/themes/orbelink/js/global.js
@@ -9,10 +9,26 @@ jQuery(document).ready(function(){
 		jQuery('.menu-desplegable').slideUp();
 	});
 	
+
+	jQuery('#blog-list-content .views-row').each(function(indice, elemento) {
+		var left = jQuery(this).css('left');
+		var alto = jQuery(this).css('height');
+		var marginBottom = jQuery(this).css('margin-bottom');
+		var paddingBottom = jQuery(this).css('padding-bottom');
+		//jQuery(this).css({'height':alto, 'margin-bottom':marginBottom, 'padding-bottom':paddingBottom});
+		if(left != '0px'){
+			jQuery(this).css({'left':'330px'});
+		}
+	  //console.log('El elemento con el índice '+indice+' contiene '+jQuery(elemento).text());
+	});	
+	
 	var $container = jQuery('#blog-list-content');
 	$container.masonry({
+		columnWidth: 315,
 		itemSelector: '.views-row'
-	});	
+	});
+	
+	
 	jQuery('#blog-list-content .views-row').addClass("hidden").viewportChecker({
 	    classToAdd: 'visible animated bounceInUp', // Class to add to the elements when they are visible
 	    offset: 90    
