b6ed0d931787bb95a79387081dc3d53bf998539b
Keylor Mora <keylor@orbelink.com>
2014-10-03 10:28:01 -0600

    css

--- sites/all/themes/orbelink/templates/view/views-view-unformatted--taxonomy-term--page.tpl.php
+++ sites/all/themes/orbelink/templates/view/views-view-unformatted--taxonomy-term--page.tpl.php
@@ -10,9 +10,10 @@
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
-
+<div id="blog-list-content">
 <?php foreach ($rows as $id => $row): ?>
   <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
     <?php print $row; ?>
   </div>
 <?php endforeach; ?>
+</div>
