08bda362c20c8dd95cc48fab0346051cd0014f9d
Andrey <andreys2009@gmail.com>
2014-10-03 16:43:56 -0600

    asda

--- sites/all/themes/orbelink/js/global.js
+++ sites/all/themes/orbelink/js/global.js
@@ -22,16 +22,16 @@ jQuery(document).ready(function(){
 	    classToAdd: 'visible animated bounceInUp', // Class to add to the elements when they are visible
 	    offset: 90    
 	   });
-	alert(jQuery(".main #blog-list-content").css('height'));
+	//alert(jQuery(".main #blog-list-content").css('height'));
 	
-	//jQuery("#blog-list-content .views-row").css({'display':'none'});
+	jQuery("#blog-list-content .views-row").css({'display':'none'});
 	var ordenId = 1;
 	jQuery(".icon_more_post").click(function(){
 	
-	for(i=ordenId;i<=(ordenId+3);i++){
+	/*for(i=ordenId;i<=(ordenId+3);i++){
 		jQuery(".views-row-"+i).css({'display':'block'});
 		ordenId = ordenId + 1;
-	}
+	}*/
 	
 	});
 });
