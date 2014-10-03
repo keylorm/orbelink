7fb1f4966afe4abb8e8569c21d8affd38758b89c
Andrey <andreys2009@gmail.com>
2014-10-03 14:36:05 -0600

    sdsdfsdfsdf

--- sites/all/themes/orbelink/js/global.js
+++ sites/all/themes/orbelink/js/global.js
@@ -1,5 +1,6 @@
 jQuery.noConflict(); 
 jQuery(document).ready(function(){
+	
 	jQuery('#boton-menu').click(function(){
 		jQuery('.menu-desplegable').slideDown();
 
@@ -8,45 +9,18 @@ jQuery(document).ready(function(){
 	jQuery('#boton-cerrar-menu').click(function(){
 		jQuery('.menu-desplegable').slideUp();
 	});
-
-	/*jQuery.ajax({
-		type: "POST",
-		url: 'http://localhost/orbelink/blog?page=1',
-		cache: false,
-		success: function (html) {
-			if (html.length > 0) {
-				var el = jQuery(html);
-				var post = jQuery(html).find("#blog-list-content").html();
-				var rows = jQuery( html ).filter('div.views-row');
-				$container.append(post).masonry( 'appended', post);		
-				
-			}
-		}
-	});	*/
-	
-	jQuery('#blog-list-content .views-row').each(function(indice, elemento) {
-		//var alto = jQuery(this).css('height');
-		//var marginBottom = jQuery(this).css('margin-bottom');
-		//var paddingBottom = jQuery(this).css('padding-bottom');
-		//jQuery(this).css({'height':alto, 'margin-bottom':marginBottom, 'padding-bottom':paddingBottom});
-		//alert(left);
-		
-	  //console.log('El elemento con el índice '+indice+' contiene '+jQuery(elemento).text());
-	});	
-	
 	
 	var cantidad_li = 0;
 	jQuery('.flex-control-nav li').each(function(indice, elemento) {
 		cantidad_li = cantidad_li + 1;
 	});
+	
 	var result = 196 - ((cantidad_li - 1) * 11);
 	jQuery('.flex-control-nav').css({'top':result});
 	
-	jQuery('#blog-list-content .views-row').addClass("hidden").viewportChecker({
+	jQuery('.masonry .views-row').addClass("hidden").viewportChecker({
 	    classToAdd: 'visible animated bounceInUp', // Class to add to the elements when they are visible
 	    offset: 90    
-	   }); 	
-	//columnWidth: 200,
-	//jQuery(".views-row").attr('data-ix','box-post');
+	   });
 
 });
