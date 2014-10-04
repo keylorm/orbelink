b0355c3fd2e8c37acd74bc7836375669b5e548b1
Andrey <andreys2009@gmail.com>
2014-10-03 18:22:12 -0600

    Css Js Listado

--- sites/all/themes/orbelink/js/global.js
+++ sites/all/themes/orbelink/js/global.js
@@ -18,27 +18,56 @@ jQuery(document).ready(function(){
 	var result = 196 - ((cantidad_li - 1) * 11);
 	jQuery('.flex-control-nav').css({'top':result});
 	
-	jQuery('#blog-list-content .views-row').addClass("hidden").viewportChecker({
+	/*jQuery('#blog-list-content .views-row').addClass("hidden").viewportChecker({
 	    classToAdd: 'visible animated bounceInUp', // Class to add to the elements when they are visible
 	    offset: 90    
-	   });
-
-	//alert(jQuery(".main #blog-list-content").css('height'));
-
-
+	   });*/
+	   
 	jQuery('.subtitle-comentario a').smoothScroll();
 
-	alert(jQuery(".main #blog-list-content").css('height'));
-	
-	jQuery("#blog-list-content .views-row").css({'display':'none'});
-	var ordenId = 1;
+	jQuery("#blog-list-content .views-row").addClass("hidden").viewportChecker({
+	    	classToAdd: 'visible animated bounceInUp',
+	    	offset: 90    
+	   });	
+
+	/*jQuery("#blog-list-content .views-row").addClass("hidden");
+	var ordenid = 1;
+	var cantv = 3;
 	jQuery(".icon_more_post").click(function(){
+	var c = ordenid + cantv;
+	for(i=ordenid;i<=c;i++){
+		var div = jQuery(".views-row-"+i);
+		
+		var a = div.outerHeight(true);
+		var l = div.position();
+		var li = 0;
+		var ld = 0;
+		if(l=='0'){
+			li = l.left + a;
+		}else{
+			ld = l.left + a;
+		}
+		alert(li);
+		
+		if(li>ld){
+			jQuery("#blog-list-content").css({'height':(li+100)+'px'});	//alert((li+100)+'px');
+		}else{
+			jQuery("#blog-list-content").css({'height':(ld+100)+'px'});//	alert((ld+100)+'px');
+		}
+		
+		
+		jQuery("#blog-list-content .views-row-"+i).viewportChecker({
+	    	classToAdd: 'visible animated bounceInUp', // Class to add to the elements when they are visible
+	    	offset: 90    
+	   });		
+	   ordenid = ordenid + 1;
+	}
 	
-	/*for(i=ordenId;i<=(ordenId+3);i++){
-		jQuery(".views-row-"+i).css({'display':'block'});
-		ordenId = ordenId + 1;
-	}*/
+	});*/
 	
-	});
+	/*setTimeout(function(){
+		jQuery("#blog-list-content").css({'height':'100px'});	
+	}, 3000);*/
+	
 
 });
