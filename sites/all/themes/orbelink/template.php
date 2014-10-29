8ddc92b1c25b19179fcd7ffdb8c868a5e5876890
juanmnl07 <juanmnl07@gmail.com>
2014-10-29 16:43:16 -0600

    configurar template.php y agregar mediaqueries

--- sites/all/themes/orbelink/template.php
+++ sites/all/themes/orbelink/template.php
@@ -91,8 +91,8 @@ function orbelink_preprocess_html(&$variables) {
     '#tag' => 'meta',
     '#attributes' => array(
       'name' => 'viewport',
-      //'content' => 'width=device-width, initial-scale=1, maximum-scale=1',
-      'content' => 'width=1024',
+      'content' => 'width=device-width, initial-scale=1, maximum-scale=1',
+      //'content' => 'width=1024',
     ),
   );
   drupal_add_html_head($meta_viewport, 'meta_viewport');
